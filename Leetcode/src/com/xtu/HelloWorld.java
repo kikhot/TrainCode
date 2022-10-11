d7258d330179fe8aa5f9e3a85ce5e98237f51e4a
quixotic1023 <1363762291@qq.com>
2022-10-11 10:16:08 +0800

    leetcode 1790, 2022/10/11

--- Leetcode/src/com/xtu/HelloWorld.java
+++ Leetcode/src/com/xtu/HelloWorld.java
@@ -3,6 +3,5 @@ package com.xtu;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
-        System.out.println("how are you?");
     }
 }
